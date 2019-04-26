d2a5292ced1d2b70d77cfcdd45fdf06f8271a0de
girin-dev <kw8799@gmail.com>
2019-04-27 02:09:20 +0900

    Header 햄버거버튼->상품,계좌 버튼으로 변경 & btn 폰트변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,16 @@ class Header extends Component {
             <div className="icon-previous" onClick={this.props.goBack}><span className="hidden">뒤로가기</span></div>
           : <div className="header-logo"><span className="hidden">디프만 로고</span></div>
         }
-        <button type="button" className="header-menu btn-menu"><span className="hidden">메인메뉴 열기</span>
+        {/* <button type="button" className="header-menu btn-menu"><span className="hidden">메인메뉴 열기</span>
           <span className="menubar menubar-top"></span>
           <span className="menubar menubar-middle"></span>
           <span className="menubar menubar-bottom"></span>
+        </button> */}
+        <button type="button" className="header-product btn-menu">
+          <img src="/assets/icon-graph.svg" alt="상품 페이지로 가기"/>
+        </button>
+        <button type="button" className="header-account btn-menu">
+          <img src="/assets/icon-account.svg" alt="내 계좌보기"/>
         </button>
         <nav></nav>
       </header>
